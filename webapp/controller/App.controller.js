f9ee011d7b110464b63b21fd7a2d64e672c36a69
MasterRomantic <70520086+MasterRomantic@users.noreply.github.com>
2023-08-23 11:56:05 +0200

    Step 6: Modules
    we replace the alert from the last exercise with a proper Message Toast from the sap.m library.

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,16 +1,14 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function (Controller) {
+    "sap/ui/core/mvc/Controller",
+    // adding a module (resource) from the sap/m library
+    //modules are loaded asyncronously
+    "sap/m/MessageToast"
+], function (Controller, MessageToast) {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
         onShowHello : function () {
-            // show an alert
-            alert("Hello world from controller!");
+            // show an alert using the MessageToast from the sap/m library
+            MessageToast.show("Hello world from a message toast from a module");
         }
     });
 });
-
-// Controller names are capitalized.
-// Controllers carry the same name as the related view (if there is a 1:1 relationship)
-// Event handlers are prefixed with on
-// Controller names always end with *.controller.js
