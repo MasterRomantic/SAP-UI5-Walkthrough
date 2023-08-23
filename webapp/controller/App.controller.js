4f9bfd8d55b498114aa2b755650af9df6814822b
MasterRomantic <70520086+MasterRomantic@users.noreply.github.com>
2023-08-23 12:26:26 +0200

    Step 7: JSON Model
    We added an input field to our app, binded its value to the model, and binded the same value to the description of the input field. The description will be directly updated as the user types.

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,14 +1,26 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    // adding a module (resource) from the sap/m library
-    //modules are loaded asyncronously
-    "sap/m/MessageToast"
-], function (Controller, MessageToast) {
+    "sap/m/MessageToast",
+    // adding the json model
+    "sap/ui/model/json/JSONModel"
+ ], function (Controller, MessageToast, JSONModel) {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-        onShowHello : function () {
-            // show an alert using the MessageToast from the sap/m library
-            MessageToast.show("Hello world from a message toast from a module");
-        }
+        // onInit is one of SAPUI5’s lifecycle methods that is invoked by the
+        // framework when the controller is created, similar to a constructor function of a control.
+       onInit : function () {
+        // set the data model on the view
+          var oData = {
+             recipient : {
+                name : "From a Json Model"
+             }
+          };
+          var oModel = new JSONModel(oData);
+          // To be able to use this model from within the XML view
+          this.getView().setModel(oModel);
+       },
+       onShowHello : function () {
+          MessageToast.show("Hello World");
+       }
     });
-});
+ });
--- webapp/index.js
+++ webapp/index.js
@@ -2,8 +2,6 @@ sap.ui.define([
     "sap/ui/core/mvc/XMLView"
 ], function (XMLView) {
     "use strict";
-    // We are now using an XMLView to display the "Hello world" text in the body tag.
-    //Views are called asyncronously, so the "then" is executed once the view has been called.
     XMLView.create({
         viewName: "sap.ui.demo.walkthrough.view.App"
     }).then(function (oView) {
