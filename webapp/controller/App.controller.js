894e852aab2a465991494eed7d1a4d534ea0394a
MasterRomantic <70520086+MasterRomantic@users.noreply.github.com>
2023-08-25 13:34:58 +0200

    Step 15: Nested Views
    Instead of putting the panel and its content directly into our App view, we move it to a new separate HelloPanel view. We refer to this using an XMLView tag in the content aggregation of the panel.

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,19 +1,6 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
-], function (Controller, MessageToast, JSONModel, ResourceModel) {
+], function (Controller) {
     "use strict";
-    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-        onShowHello: function () {
-            // read message from i18n model
-            var oBundle = this.getView().getModel("i18n").getResourceBundle();
-            var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-            var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-
-            // Show the message
-            MessageToast.show(sMsg);
-        }
-    });
+    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {});
 });
