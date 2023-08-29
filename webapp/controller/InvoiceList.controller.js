97d6f902c5f7c83d7037f2ee14d43030579603ef
MasterRomantic <70520086+MasterRomantic@users.noreply.github.com>
2023-08-29 10:38:45 +0200

    Step 22: Custom Formatters
    We now add a localized status with a custom formatter, because the status in our data model is in a technical format.

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -1,10 +1,12 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
-], function (Contoller, JSONModel) {
+    "sap/ui/model/json/JSONModel",
+    "../model/formatter"
+], function (Contoller, JSONModel, formatter) {
     'use strict';
 
     return Contoller.extend("sap.ui.demo.walkthrough.controller.InvoiceList", {
+        formatter: formatter,
         onInit: function () {
             var oViewModel = new JSONModel({
                 currency: "EUR"
