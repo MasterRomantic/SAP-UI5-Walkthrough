feac0be4af833352b15be7dcc24d6a0872487367
MasterRomantic <70520086+MasterRomantic@users.noreply.github.com>
2023-09-04 23:37:41 +0200

    Step 30: Routing and Navigation
    We use the SAPUI5 navigation features to load and show a separate detail page that we can later use to display details for an invoice.

--- Step29ReadMe.md
+++
@@ -1,5 +0,0 @@
-# Step 29: Debugging Tools
-
-If you use the Google Chrome browser, you can install the UI5 Inspector plugin. With this plugin, you can easily debug your SAPUI5- or OpenUI5-based apps. For more information, see UI5 Inspector.
-
-Use the shortcut Ctrl + Shift + Alt / Option + S to open the SAPUI5 support diagnostics tool.
--- webapp/component.js
+++ webapp/component.js
@@ -1,25 +1,30 @@
 sap.ui.define([
-    "sap/ui/core/UIComponent",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
- ], function (UIComponent, JSONModel, ResourceModel) {
-    "use strict";
-    return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
-       metadata : {
-          "interfaces": ["sap.ui.core.IAsyncContentCreation"],
-          "manifest": "json"
-       },
-       init : function () {
-          UIComponent.prototype.init.apply(this, arguments);
+   "sap/ui/core/UIComponent",
+   "sap/ui/model/json/JSONModel",
+   "sap/ui/model/resource/ResourceModel"
+], function (UIComponent, JSONModel, ResourceModel) {
+   "use strict";
+   return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
+      metadata: {
+         "interfaces": ["sap.ui.core.IAsyncContentCreation"],
+         "manifest": "json"
+      },
+      init: function () {
+         UIComponent.prototype.init.apply(this, arguments);
 
-          // set data model
-          var oData = {
-             recipient : {
-                name : "World"
-             }
-          };
-          var oModel = new JSONModel(oData);
-          this.setModel(oModel);
-       }
-    });
- });    
+         // set data model
+         var oData = {
+            recipient: {
+               name: "World"
+            }
+         };
+         var oModel = new JSONModel(oData);
+         this.setModel(oModel);
+
+         // create the views based on the url/hash.
+         // it is automatically instantiated based on our 
+         // AppDescriptor configuration and assigned to the component.
+         this.getRouter().initialize();
+      }
+   });
+});    
